747ac0842b13c63cc8abb2075dd5a373edb0caff
Gustavo Nogueira <122294673+gnogueira-lgc@users.noreply.github.com>
2024-01-04 16:25:42 +0000

    Update look and feel

--- src/popup.js
+++ src/popup.js
@@ -4,49 +4,26 @@ import './popup.css';
 const { v4: uuidv4 } = require('uuid');
 
 (function() {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
-
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: cb => {
-      chrome.storage.sync.get(['count'], result => {
-        cb(result.count);
-      });
-    },
-    set: (value, cb) => {
-      chrome.storage.sync.set(
-        {
-          count: value,
-        },
-        () => {
-          cb();
-        }
-      );
-    },
-  };
-  
   document.addEventListener('DOMContentLoaded', () => {
   const navbar = document.querySelector('.navbar');
-  const expandIcon = document.createElement('span');
-  expandIcon.classList.add('expand-icon');
 
   navbar.addEventListener('click', (event) => {
     if (event.target.classList.contains('nav-link')) {
       const collapseId = event.target.getAttribute('href');
       const collapseElement = document.querySelector(collapseId);
-      collapseElement.classList.toggle('collapse');
 
-      event.target.classList.toggle('active');
-      expandIcon.classList.toggle('rotate');
+      if (!event.target.classList.contains("active")) {
+        // Collapses all pages
+        [...document.querySelectorAll(".navbar .nav-link")].forEach((el) => {
+          el.classList.remove("active");
+          document.querySelector(el.getAttribute('href')).classList.add("collapse");
+        });
+
+        // Show only the active page
+        event.target.classList.add('active');
+        collapseElement.classList.remove('collapse');
+      }
     }
-  });
-
-  expandIcon.addEventListener('click', () => {
-    navbar.querySelector('.nav-link.active').click();
   });
 });
 
